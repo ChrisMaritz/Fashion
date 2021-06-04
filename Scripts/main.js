238fc7aafc4ad8b2099ff12ec26265fb340793af
ChrisMaritz <chrismaritz.based@gmail.com>
2021-06-04 16:30:50 +0200

    version 6'00

---
+++ Scripts/main.js
@@ -0,0 +1,8 @@
+// google-map-about-page
+function initMap() {
+    const location = {lat: -33.929, lng: 18.410};
+    const map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 12,
+        center: location
+    });
+}
