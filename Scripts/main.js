a6b562910310cbd862c434f5c6b2ee119014085e
ChrisMaritz <chrismaritz.based@gmail.com>
2021-09-13 12:22:42 +0200

    final

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,234 +6,7 @@ function initMap() {
         center: location
     });
 }
-//cart page
 
-//targeting shop items
-let carts = document.querySelectorAll("#add-cart")
-
-//each item product specfications
-let products = [
-    {
-        name : "MUSTARD SKIRT",
-        tag : "mustardskirt",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "OLIVE JERSEY",
-        tag : "olivejersey",
-        price : 250,
-        inCart : 0,
-    },
-    {
-        name : "PINK JACKET",
-        tag :"pinkjacket",
-        price : 400,
-        inCart : 0,
-    },
-    {
-        name : "RED JERSEY",
-        tag : "redjersey",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "WAISTCOAT",
-        tag : "waistcoat",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "STRIPED PANTS",
-        tag : "stripedpants",
-        price : 200,
-        inCart : 0
-    },
-    {
-        name : "WOOL JACKET",
-        tag : "wooljacket",
-        price : 500,
-        inCart : 0
-    },
-    {
-        name : "FLUFFY JERSEY",
-        tag : "fluffyjersey",
-        price : 350,
-        inCart : 0
-    },
-    {
-        name : "YELLOW JOGGERS",
-        tag : "yellowjersey",
-        price : 200,
-        inCart : 0
-    },
-    {
-        name : "MUSTARD T-SHIRT",
-        tag : "mustardt-shirt",
-        price : 200,
-        inCart : 0
-    },
-    {
-        name : "LEAPOARD JACKET",
-        tag : "leapoardjacket",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "CORDUROY PANTS",
-        tag : "corduroypants",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "CAPPUCCINO SKIRT",
-        tag : "cappucinoskirt",
-        price : 250,
-        inCart : 0
-    },
-    {
-        name : "THIN BLOUSE",
-        tag : "thinblouse",
-        price : 200,
-        inCart : 0
-    },
-    {
-        name : "DENIM JACKET",
-        tag : "denimjacket",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "DOTTED SKIRT",
-        tag : "dottedskirt",
-        price : 350,
-        inCart : 0
-    },
-    {
-        name : "LIGHT DENIM JACKET",
-        tag : "lightdenimjacket",
-        price : 350,
-        inCart : 0
-    },
-    {
-        name : "LOOSE DENIM PANTS",
-        tag : "loosedenimpants",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "CLASSIC DENIM PANTS",
-        tag : "classicdenimpants",
-        price : 300,
-        inCart : 0
-    },
-    {
-        name : "STRIPED SHIRT",
-        tag : "stripedpants",
-        price : 200,
-        inCart : 0
-    }
-]
-
-
-//loop for adding cart items
-// for (let i=0; i < carts.length; i++){
-//     carts[i].addEventListener("click", () =>{
-//         cartNumbers(products[i]);
-//         totalCost(products[i]);
-//     })
-// }
-
-
-//onload amount of cart items
-// function onLoadCartNumbers(){
-//     let productNumbers = localStorage.getItem("cartNumbers");
-
-//     if (productNumbers){
-//         document.querySelector("#cart span").textContent = productNumbers
-//     }
-// }
-
-//item amount in cart set and text content
-// function cartNumbers(product){
-//     let productNumbers = localStorage.getItem("cartNumbers");
-//     productNumbers = parseInt(productNumbers)
-//     if( productNumbers ){
-//         localStorage.setItem("cartNumbers", productNumbers  + 1);
-//         document.querySelector("#cart span").textContent = productNumbers + 1
-//     } else {
-//         localStorage.setItem("cartNumbers", 1);
-//         document.querySelector("#cart span").textContent = 1
-//     }
-//     setItems(product);
-// }
-
-
-//setting items for products specifications
-// function setItems(product){
-//     let cartItems = localStorage.getItem("productsInCart");
-//     cartItems = JSON.parse(cartItems);
-//     if(cartItems != null){
-//         if(cartItems[products.tag] == undefined){
-//             cartItems = {
-//                 ...cartItems,
-//                 [product.tag] : product
-//             }
-//         }
-//         cartItems[product.tag].inCart += 1;
-//     }else{
-//         product.inCart = 1
-//         cartItems = {
-//             [product.tag] : product
-//         }
-//     }
-//     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
-// }
-
-
-// //total cost setting 
-// function totalCost(product){
-//     let cartCost = localStorage.getItem("totalCost");
-//     if (cartCost != null){
-//         cartCost = parseInt(cartCost);
-//         localStorage.setItem("totalCost", cartCost + product.price);
-//     }else{
-//         localStorage.setItem("totalCost", product.price);
-//     }
-// }
-
-
-// //price details display in cart
-// function cartDisplayPrice(){
-//     let cartItems = localStorage.getItem("productsInCart");
-//     cartItems = JSON.parse(cartItems);
-//     let result = Object.values(cartItems).filter(obj => {
-//         let itemName = obj.name;
-//         let itemPrice = obj.price;
-//         console.log(itemName, "hello");
-//         document.getElementById("priceDisplay").append(itemName,"(","R",itemPrice,")"," ");
-//     });
-// }
-
-
-// //items names display
-// function cartDisplayName(){
-//     let cartItems = localStorage.getItem("productsInCart");
-//     cartItems = JSON.parse(cartItems);
-//     let result = Object.values(cartItems).filter(obj => {
-//         let itemName = obj.name;
-//         let itemAmount = obj.inCart;
-//         document.getElementById("nameDisplay").append(itemName,"(",itemAmount,")",",");
-//     });
-// }
-
-
-// //total cost display
-// function cartDisplayTotal(){
-//     let cartItems = localStorage.getItem("totalCost");
-//     cartItems = JSON.parse(cartItems);
-//     document.getElementById("totalDisplay").append("R",cartItems);
-// }
 new Vue ({
     el : "#whole_page",
 
@@ -286,7 +59,6 @@ new Vue ({
                 }
 
                 let object = new item (key, name, price);
-                console.log(typeof(this.cartItems));
                 this.cartItems.push(object);
             localStorage.setItem("cartItems", JSON.stringify(this.cartItems));
 
@@ -297,9 +69,6 @@ new Vue ({
 
             this.total = parseInt(localStorage.getItem("cartAmount"));
             
-        },
-        test(){
-            console.log("hello")
         },
         value(){
             return localStorage.getItem("cartItems")
@@ -331,13 +100,6 @@ new Vue ({
         let newObject =object.map(this.fullCost);
         let sum = (newObject.reduce(this.getSum, 0))
         this.theSum = sum;
-        console.log(sum);
-        // sum = 0; 
-        // let getPrices = JSON.parse(localStorage.getItem("cartItems"));
-        // for (let key in getPrices){
-        //     sum += getPrices[key.prices]
-        // }
-        // console.log(sum);
     },
     methods : {
         fullCost(object){
@@ -348,8 +110,3 @@ new Vue ({
           }
     },
 })
-//onload items
-// cartDisplayName();
-// cartDisplayPrice();
-// cartDisplayTotal();
-// onLoadCartNumbers();
